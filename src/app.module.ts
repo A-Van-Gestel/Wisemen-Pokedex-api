dfb016c18b73acacc8da34287e4fd4847922c4fb
Axel Van Gestel <axel.van.gestel@gmail.com>
2024-04-01 21:03:06 +0200

    Deleted: App: Default app controller & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DbModule } from './db';
 import { PokemonsModule } from './pokemon';
 
 @Module({
   imports: [DbModule, PokemonsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
